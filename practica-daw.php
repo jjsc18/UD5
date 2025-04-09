b527abd564f41300b68222144f297aa6e1e2b84e
Sanchez Cespedes, Juan Jose <jjsc18@yahoo.es>
2025-04-09 10:05:07 +0200

    primer commit

---
+++ practica-daw.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Función que suma dos números.
+ *
+ * @param int $a El primer número.
+ * @param int $b El segundo número.
+ * @return int La suma de los dos números.
+ */
+function suma($a, $b) {
+    return $a + $b;
+}
+?>
